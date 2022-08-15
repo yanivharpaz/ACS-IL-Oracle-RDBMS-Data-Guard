93b44c3e854417c1ef3d8da3e42760068afaf909
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-08-15 13:55:08 +0300

    copy scripts to /tmp

--- example_generated_files/450_rman_connect_and_restore.sh
+++ example_generated_files/450_rman_connect_and_restore.sh
@@ -1,1 +1,1 @@
-rman TARGET sys/Ora_Passwd1@CDB1 AUXILIARY sys/Ora_Passwd1@CDB1_STBY cmdfile=/tmp/rman_restore_dg.rman
+rman TARGET sys/Ora_Passwd1@CDB1 AUXILIARY sys/Ora_Passwd1@ORACLE_SID_STBY cmdfile=/tmp/rman_restore_dg.rman
