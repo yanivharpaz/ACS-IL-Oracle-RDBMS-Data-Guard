9334893f8e3775427ac4fe52cef23da4db18a501
Yaniv Harpaz <yanivharpaz@users.noreply.github.com>
2022-08-17 13:08:20 +0300

    open port

--- scripts/100_prep_dg_files.sh
+++ scripts/100_prep_dg_files.sh
@@ -16,6 +16,8 @@ cp -pvf $SCRIPT_DIR/oracle_rdbms_config_sample.conf /tmp
 cp -pvf $SCRIPT_DIR/190_update_db_config.sh /tmp
 cp -pvf $SCRIPT_DIR/200_primary_dg_setup.sh /tmp
 cp -pvf $SCRIPT_DIR/300_standby_dg_setup.sh /tmp
+cp -pvf $SCRIPT_DIR/110_open_port.sh /tmp
+
 chmod 666 /tmp/oracle_rdbms_config_sample.conf
 chmod 777 /tmp/190_update_db_config.sh
 chmod 777 /tmp/200_primary_dg_setup.sh
